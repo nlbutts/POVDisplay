cc21a42d5cb55e54acf252b047671e5a1a7b7ec6
Nicholas Butts <nlbutts@ieee.org>
2016-12-28 16:16:25 -0600

    Updated schematic and removed some old datasheets.

--- POVRotate.cydsn/main.c
+++ POVRotate.cydsn/main.c
@@ -10,44 +10,20 @@
  * ========================================
 */
 #include <project.h>
-
-void spiLedFlish()
-{
-    volatile uint8_t bytesInBuf = SPI_LED_SpiUartGetTxBufferSize();
-    while (SPI_LED_SpiUartGetTxBufferSize() > 0) {};   
-}
-
-
-void ledControl(uint8_t numLED, uint8_t global, uint32_t rgb)
-{
-    uint8_t start[4] = {0, 0, 0, 0};
-    uint8_t stop[4]  = {0xFF, 0xFF, 0xFF, 0xFF};
-    uint8_t led;
-
-    SPI_LED_SpiUartPutArray(start, 4);
-
-    for (led = 0; led < numLED; led++)
-    {
-        uint8_t data[4];
-        data[0] = 0xE0 | global;
-        data[1] = (rgb      ) & 0xFF;
-        data[2] = (rgb >>  8) & 0xFF;
-        data[3] = (rgb >> 16) & 0xFF;
-        SPI_LED_SpiUartPutArray(data, 4);
-    }
-
-    SPI_LED_SpiUartPutArray(stop, 4);
-    spiLedFlish();
-}
+#include "LED.h"
 
 int main()
 {
     uint8_t ledState = 0;
-    uint32_t color = 0xFF;
     CyGlobalIntEnable; /* Enable global interrupts. */
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     SPI_LED_Start();
+    I2C_EEPROM_Start();
+
+    LED_S cfg;
+    cfg.numberOfLeds = 8;
+    led_init(&cfg);    
 
     for(;;)
     {
@@ -55,12 +31,15 @@ int main()
         STATUS_LED_Write(ledState);
         ledState = !ledState;
         CyDelay(1000);
-        ledControl(8, 5, color);
-        color <<= 8;
-        if (color > 0xFF0000)
-        {
-            color = 0xFF;
-        }
+        led_setColor(OFF);
+        CyDelay(100);
+        led_setColor(RED);
+        CyDelay(100);
+        led_setColor(GREEN);
+        CyDelay(100);
+        led_setColor(BLUE);
+        CyDelay(100);
+        led_setColor(WHITE);
     }
 }
 
